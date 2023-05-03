5cbb79246dbaf8c56e903476b9d7d4a8b2f5bc42
Desmond <desmondmies@gmail.com>
2023-05-03 15:10:16 +0200

    Ajout app JavaFX basique
    Crée une app javaFX basique avec possibilité de rentrer un numéro de fiche
    (dont une erreur si le numéro n'est pas au format valide)
    Pour l'instant l'appli redirige l'utilisateur automatiquement vers une page de visualisation d'épigraphie,
    en prévention de pouvoir récupérer le fichier XML et visualiser l'épigraphe en question.

--- frontend/src/main/java/fr/univtln/m1infodid/projets2/App.java
+++ frontend/src/main/java/fr/univtln/m1infodid/projets2/App.java
@@ -1,5 +1,7 @@
 package fr.univtln.m1infodid.projets2;
 
+import fr.univtln.m1infodid.projets2.javafx.app.MainApp;
+import javafx.application.Application;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -9,5 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 public class App {
     public static void main ( String[] args ) {
         log.info("Hello World!");
+        Application.launch(MainApp.class); //lance l'application javaFX
     }
 }
---
+++ frontend/src/main/java/fr/univtln/m1infodid/projets2/javafx/controller/MenuController.java
@@ -0,0 +1,69 @@
+package fr.univtln.m1infodid.projets2.javafx.controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Classe logique de la vue du menu
+ */
+@Slf4j
+public class MenuController implements Initializable {
+
+    private static final String NUMBER_ONLY_REGEX = "\\d+"; //[0-9]+
+
+    @FXML private AnchorPane anchorPane;
+    @FXML private TextField inputBar;
+
+    @FXML private Parent alert;
+    @FXML private AlertController alertController;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        alert.setDisable(true);
+        alertController.hideAlertPane();
+
+        Platform.runLater( () -> anchorPane.requestFocus() );
+        inputBar.setText("");
+        anchorPane.widthProperty().addListener( (obs, oldValue, newValue) -> alertController.setScreenWidth(newValue.doubleValue()) );
+    }
+
+    /**
+     * Méthode appelé lorsque le bouton de numéro de fiche est cliqué.
+     * Affiche une erreur si l'entrée de l'utilisateur n'est pas valide
+     * Sinon le numéro de fiche est récupérer
+     * @throws IOException renvoi une erreur si il y a un soucis pour le chargement de la nouvelle scène
+     */
+    @FXML private void numFicheBtnOnClick() throws IOException {
+
+        if ( !isInputOnlyInteger(inputBar.getText()) ) {
+            alert.setDisable(false);
+            alertController.showNotValidId();
+            return;
+        }
+
+        int validId = Integer.parseInt( inputBar.getText() );
+        log.info("VALID ID : " + validId);
+        //appeler la méthode pour récupérer le fichier XML à partir de cet id
+
+        //pour le test uniquement, changement de scène auto, à supprimer plus tard
+        Stage primaryStage = (Stage) anchorPane.getScene().getWindow();
+        SceneController.switchToPageVisualisation( primaryStage );
+    }
+
+    /**
+     * Vérifie si l'entrée est valide selon le regex, celle ci vérifie que l'entrée est bien un nombre
+     * @param input la chaîne de caractère à tester
+     * @return le résultat du regex, boolean
+     */
+    private boolean isInputOnlyInteger(String input) { return input.matches(NUMBER_ONLY_REGEX); }
+}
