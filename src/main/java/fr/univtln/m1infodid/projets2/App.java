3ec45452c84ae6fed6c36fc251532d41bf74265f
Tom D <tom-domenge@etud.univ-tln.fr>
2023-05-03 12:41:21 +0200

    Projet de base maven
    Intégration de la CI avec sonar cloud

---
+++ src/main/java/fr/univtln/m1infodid/projets2/App.java
@@ -0,0 +1,13 @@
+package fr.univtln.m1infodid.projets2;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Hello world!
+ */
+@Slf4j
+public class App {
+    public static void main ( String[] args ) {
+        log.info("Hello World!");
+    }
+}
---
+++ src/test/java/fr/univtln/m1infodid/projets2/AppTest.java
@@ -0,0 +1,11 @@
+package fr.univtln.m1infodid.projets2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+ class AppTest  {
+    @Test
+    public void HelloTest() {
+        Assertions.assertTrue(true);
+    }
+}
