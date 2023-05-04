8f910c4a726f1cfa2e48c49bf7dcb44cf0faaef2
benhammanemeryam <meryambenhammane532@gmail.com>
2023-05-04 14:12:33 +0200

    ajout classe SI de méthodes récuperation fichier XML

---
+++ src/main/java/fr/univtln/m1infodid/projets2/App.java
@@ -0,0 +1,18 @@
+package fr.univtln.m1infodid.projets2;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ */
+@Slf4j
+public class App {
+    public static void main(String[] args) {
+        String id = "340";
+        String xmlUrl = "http://ccj-epicherchel.huma-num.fr/interface/fiche_xml2.php?id=" + id;
+        ArrayList<String> contentdImageEtText = SI.extractTextAndImageFromXml(id, xmlUrl);
+        System.out.println(contentdImageEtText);
+    }
+}
