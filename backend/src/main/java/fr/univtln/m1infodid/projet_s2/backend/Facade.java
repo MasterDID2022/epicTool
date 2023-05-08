bf37e6e34a6524637cc4c45cc24c3b96faf17e7f
Theo-Hafsaoui <theo-hafsaoui@etud.univ-tln.fr>
2023-05-08 15:53:15 +0200

    Ajout SI
    Ajout d'une classe Épigraphe ainsi que d'une classe SI qui prend un XML d'épigraphe et retourne différent attribut
    
    Co-authored-by: rawi124 <rawia.ben.amira1997@gmail.com>
    Co-authored-by: benhammanemeryam <meryambenhammane532@gmail.com>

---
+++ backend/src/main/java/fr/univtln/m1infodid/projet_s2/backend/Facade.java
@@ -0,0 +1,45 @@
+package fr.univtln.m1infodid.projet_s2.backend;
+
+import fr.univtln.m1infodid.projet_s2.backend.exceptions.*;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+public class Facade {
+    private static Logger log = Logger.getLogger(Facade.class.getName());
+    /**
+     * @param id l'id de la fiche
+     * @param xmlUrl l'url de la fiche
+     * @return une instance de la classe Epigraphe contenant les informations extraites de la fiche XML correspondante
+     */
+
+    public Epigraphe createEpigraphieInstanceFromXml(String id, String xmlUrl)  {
+        ArrayList<String> contentList = null;
+        try {
+            contentList = SI.extractTextAndImageFromXml(id, xmlUrl);
+        } catch (UrlInvalide u) {
+            log.warning(u.getMessage());
+        } catch (SaxErreur s) {
+            log.warning(s.getMessage());
+        } catch (DomParser d) {
+            log.warning(d.getMessage());
+        } catch (ExtractionXml e) {
+            log.warning(e.getMessage());
+        } catch (RecuperationXml r) {
+            log.warning(r.getMessage());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        Epigraphe ep = Epigraphe.of();
+        try {
+            ep = SI.CreateEpigraphie(contentList);
+
+        } catch (ListeVide l) {
+            log.warning(l.getMessage());
+        }
+        return ep ;
+    }
+
+
+
+}
