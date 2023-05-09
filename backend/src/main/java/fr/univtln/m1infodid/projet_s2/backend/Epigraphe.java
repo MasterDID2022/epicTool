2525f10dcc887cd1481a92ae268ac8e9286a70c7
Tom D <tom-domenge@etud.univ-tln.fr>
2023-05-09 11:41:16 +0200

    Reformatage

--- backend/src/main/java/fr/univtln/m1infodid/projet_s2/backend/Epigraphe.java
+++ backend/src/main/java/fr/univtln/m1infodid/projet_s2/backend/Epigraphe.java
@@ -4,15 +4,13 @@ import java.util.Date;
 
 public class Epigraphe {
     private int id;
-    private String imgUrl ;
+    private String imgUrl;
     private Date date;
     private String texte;
     private String traduction;
     private String nom;
-    public static Epigraphe off(int id, Date date, String texte, String traduction, String nom, String imgUrl) {
-        return new Epigraphe(id, date, texte, traduction, nom, imgUrl);
-    }
-    private Epigraphe(int id, Date date, String texte, String traduction, String nom, String imgUrl) {
+
+    private Epigraphe ( int id, Date date, String texte, String traduction, String nom, String imgUrl ) {
         this.id = id;
         this.date = date;
         this.texte = texte;
@@ -20,57 +18,70 @@ public class Epigraphe {
         this.nom = nom;
         this.imgUrl = imgUrl;
     }
-    private Epigraphe() {
+
+    private Epigraphe () {
     }
-    public static Epigraphe of() {
+
+    public static Epigraphe off ( int id, Date date, String texte, String traduction, String nom, String imgUrl ) {
+        return new Epigraphe(id, date, texte, traduction, nom, imgUrl);
+    }
+
+    public static Epigraphe of () {
         return new Epigraphe();
     }
     // Getters and setters
 
-    public int getId() {
+    public int getId () {
         return id;
     }
-    public String getImgUrl() {
-        return imgUrl;
-    }
-    public void setImgUrl(String imgUrl) {
-        this.imgUrl = imgUrl;
-    }
 
-    public void setId(int id) {
+    public void setId ( int id ) {
         this.id = id;
     }
 
-    public Date getDate() {
+    public String getImgUrl () {
+        return imgUrl;
+    }
+
+    public void setImgUrl ( String imgUrl ) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Date getDate () {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate ( Date date ) {
         this.date = date;
     }
 
-    public String getTexte() {
+    public String getTexte () {
         return texte;
     }
 
-    public void setTexte(String texte) {
+    public void setTexte ( String texte ) {
         this.texte = texte;
     }
-    public String getTraduction() {
+
+    public String getTraduction () {
         return traduction;
     }
-    public void setTraduction(String traduction) {
+
+    public void setTraduction ( String traduction ) {
         this.traduction = traduction;
     }
-    public String getNom() {
+
+    public String getNom () {
         return nom;
     }
-    public void setNom(String nom) {
+
+    public void setNom ( String nom ) {
         this.nom = nom;
     }
+
     @Override
-    public String toString() {
-        return "Epigraphie : n°" + getId() + ", " + getNom() + ", " + getTexte()+ ", " + getDate()+ ", " + getImgUrl()+ ", " + getTraduction();
+    public String toString () {
+        return "Epigraphie : n°" + getId() + ", " + getNom() + ", " + getTexte() + ", " + getDate() + ", " + getImgUrl() + ", " + getTraduction();
     }
 }
 
--- backend/src/main/java/fr/univtln/m1infodid/projet_s2/backend/model/Epigraphe.java
+++ backend/src/main/java/fr/univtln/m1infodid/projet_s2/backend/model/Epigraphe.java
@@ -1,9 +1,13 @@
 package fr.univtln.m1infodid.projet_s2.backend.model;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.hibernate.Hibernate;
 
 import java.util.Date;
@@ -18,7 +22,7 @@ public class Epigraphe {
     @Id
     @Column(name = "ID")
     private int id;
-    private String imgUrl ;
+    private String imgUrl;
     private Date date;
     private String translation;
     private String name;
@@ -36,7 +40,8 @@ public class Epigraphe {
     public int hashCode () {
         return getClass().hashCode();
     }
-    public String toString() {
-        return "Epigraphie : n°" + getId() + ", " + getName() + ", " + getText()+ ", " + getDate()+ ", " + getImgUrl()+ ", " + getTranslation();
+
+    public String toString () {
+        return "Epigraphie : n°" + getId() + ", " + getName() + ", " + getText() + ", " + getDate() + ", " + getImgUrl() + ", " + getTranslation();
     }
 }
