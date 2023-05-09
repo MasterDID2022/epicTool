df811907ac9cfab755a5f2dee7f28daa14095fdc
hajar <helmoukh455@tln.fr>
2023-05-09 16:46:31 +0200

    Ajout Traduction UI
    Recuperer un texte et l'afficher sur l'interface en haut de l'image
    
    Co-authored-by: khebtani <khebtanidorsaf@hotmail.com>
    Co-authored-by: Desmondmies <48258252+Desmondmies@users.noreply.github.com>

--- frontend/src/main/java/fr/univtln/m1infodid/projet_s2/frontend/javafx/controller/PageVisualisationController.java
+++ frontend/src/main/java/fr/univtln/m1infodid/projet_s2/frontend/javafx/controller/PageVisualisationController.java
@@ -1,11 +1,18 @@
 package fr.univtln.m1infodid.projet_s2.frontend.javafx.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,27 +21,24 @@ import java.util.ResourceBundle;
  */
 public class PageVisualisationController implements Initializable {
     @FXML
+    private StackPane stackPane;
+    @FXML
     private AnchorPane anchorPane;
     @FXML
     private ImageView plaqueImg;
+    @FXML
+    private Parent traduction;
+    @FXML
+    private TradController traductionController;
 
     @Override
-    public void initialize ( URL arg0, ResourceBundle arg1 ) {
+    public void initialize(URL arg0, ResourceBundle arg1) {
 
-        //test pour l'affichage de l'image, à supprimer
-        setupVisualEpigraphe("13",
-                "http://ccj-epicherchel.huma-num.fr/interface/phototheque/13/40341.jpg",
-                "IACENTVS•CORPOR\nRE•QVSTOS•H•S•E•\nNATOS•FECERVNT",
-                "Iacentus, garde du corps. Il repose ici. Ses enfants ont fait faire (sa tombe).");
     }
 
-    public void setupVisualEpigraphe ( String id, String imgUrl, String plaqueTxt, String tradTxt ) {
-        //afficher l'id et d'autres infos de l'épigraphie sur la page
-        //obtenir l'image et l'afficher
-        //construire le clavier à partir du texte de la plaque
-        //modifier le label pour afficher la traduction
-
-        //obtiens l'image et l'affiche (TEST !!!!)
+    public void setupVisualEpigraphe(String id, String imgUrl, String plaqueTxt, String tradTxt) {
         plaqueImg.setImage(new Image(imgUrl));
+        traductionController.displayTranslation(tradTxt);
+
     }
 }
