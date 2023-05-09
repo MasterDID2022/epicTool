89791dae29e9fc3e19cb16c30a869dcee84cc693
Theo-Hafsaoui <theo-hafsaoui@etud.univ-tln.fr>
2023-05-09 21:12:38 +0200

    Annotation pour API back
    Ajout du support des annotations pour l'API du backend, update
    importante sur le GET qui devient un vrai epigraphe.
    
    Co-authored-by: sayfkd <sayfkd30@gmail.com>

--- backend/src/main/java/fr/univtln/m1infodid/projet_s2/backend/server/Api.java
+++ backend/src/main/java/fr/univtln/m1infodid/projet_s2/backend/server/Api.java
@@ -1,9 +1,13 @@
 package fr.univtln.m1infodid.projet_s2.backend.server;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import fr.univtln.m1infodid.projet_s2.backend.model.Epigraphe;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.univtln.m1infodid.projet_s2.backend.DAO.EpigrapheDAO;
 import fr.univtln.m1infodid.projet_s2.backend.SI;
+import fr.univtln.m1infodid.projet_s2.backend.model.Annotation;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Persistence;
 import jakarta.ws.rs.*;
@@ -13,66 +17,93 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.*;
+
 /**
  * Api REST pour les epigraphe
  */
 @Slf4j
 @Path("epigraphe")
 public class Api {
-    static final String URL_EPICHERCHELL = "http://ccj-epicherchel.huma-num.fr/interface/fiche_xml2.php?id=";
+	static final String URL_EPICHERCHELL = "http://ccj-epicherchel.huma-num.fr/interface/fiche_xml2.php?id=";
 
-    /**
-     * Methode static pour recupere le fichier XML de l'épigraphe d'ID id
-     * Retourne la chaine vide en cas d'échec
-     *
-     * @param id
-     * @return
-     */
-    public static String getEpigrapheOf ( Integer id ) {
-        Client client = ClientBuilder.newClient();
-        String epigraph = client.target(URL_EPICHERCHELL + id.toString())
-                .request(MediaType.APPLICATION_XML)
-                .get(String.class);
-        client.close();
-        return SI.getFirstImgUrl(epigraph);
-    }
+	/**
+	 * Methode static pour recupere le fichier XML de l'épigraphe d'ID id
+	 * Retourne la chaine vide en cas d'échec
+	 *
+	 * @param id
+	 * @return
+	 */
+	public static String getEpigrapheOf(Integer id) {
+		Client client = ClientBuilder.newClient();
+		String epigraph = client.target(URL_EPICHERCHELL + id.toString())
+				.request(MediaType.APPLICATION_XML)
+				.get(String.class);
+		client.close();
+		return SI.getFirstImgUrl(epigraph);
+	}
 
-    /**
-     * Methode qui gere les requete REST post pour les epigraphie
-     * WIP Methode jou buer pour infra
-     * Prend l'ID et l'envoie a la DAO pour la persister
-     *
-     * @return String de message de retour
-     */
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getIt ( String epigraheJson ) {
-        log.info(epigraheJson);
-        try (EntityManager emf = Persistence.createEntityManagerFactory("epigraphs").createEntityManager();
-             EpigrapheDAO dao = EpigrapheDAO.create(emf)) {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(epigraheJson);
-            JsonNode id = jsonNode.get("ID");
-            if (id != null) {
-                dao.persistID(id.asInt());
-            } else {
-                return Response.status(Response.Status.BAD_REQUEST)
-                        .entity("Il ne s'agit pas d'un epigrahe," +
-                                "l'ID")
-                        .build();
-            }
+	private Optional<Annotation> createAnnotation(String annotationJson) {
+		Optional<Annotation> annotation = Optional.empty();
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode rootNode = null;
+		try {
+			rootNode = objectMapper.readTree(annotationJson);
+			String idAnnotation = rootNode.path("idAnnotation").asText();
+			String idEpigraphe = rootNode.path("idEpigraphe").asText();
+			annotation = Optional.of(
+					Annotation.of(Integer.parseInt(idAnnotation), Integer.parseInt(idEpigraphe)));
+			JsonNode annotations = rootNode.get("annotations");
+			for (JsonNode jsonPoints : annotations) {
+				for (JsonNode coordonner : jsonPoints) {
+					Integer y = (Integer.parseInt(String.valueOf(coordonner.toString().charAt(1))));
+					Integer x = (Integer.parseInt(String.valueOf(coordonner.toString().charAt(3))));
+					annotation.get().addPoints(x, y);
+				}
+			}
+			return annotation;
+		} catch (JsonProcessingException e) {
+			log.error("Parsing error, le json ne semble pas valide");
+		}
+		return annotation;
+	}
 
-            return Response.ok().build();
-        } catch (Exception e) {
-            log.debug(e.toString());
-            return Response.serverError().entity("ERR:").build();
-        }
-    }
+	/**
+	 * Methode pour ajouter une anotation
+	 *
+	 * @param annotationJson l'anotation au format Json
+	 */
+	@Path("annotation")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response receiveAnnotation(String annotationJson) {
+		Optional<Annotation> annotation = createAnnotation(annotationJson);
+		if (annotation.isPresent()){
+			log.info(annotation.toString());// Pour DAO persister ici
+			return Response.ok().build();
+		}
+		return Response.notModified().build();
+	}
 
-    @GET
-    @Path("/{id}")
-    public Response getId ( @PathParam("id") String id ) {
-        return Response.status(200).entity(getEpigrapheOf(Integer.valueOf(id))).build();
-    }
+	/**
+	 * Methode de reponse pour une demande du front sur un epigraphe d'ID
+	 * 
+	 * @param id d'un epigraphe
+	 */
+	@GET
+	@Path("/{id}")
+	public Response sendEpigraphe(@PathParam("id") String id) throws Exception {
+		ObjectMapper objectMapper = new ObjectMapper();
+		ObjectNode responseJson = objectMapper.createObjectNode();
+		Epigraphe epigraphe = SI.CreateEpigraphie(
+				SI.extractTextAndImageFromXml(id, URL_EPICHERCHELL + Integer.valueOf(id).toString()));
+		responseJson.put("id", epigraphe.getId());
+		responseJson.put("date", String.valueOf(epigraphe.getDate()));
+		responseJson.put("texte", epigraphe.getText());
+		responseJson.put("traduction", epigraphe.getTranslation());
+		responseJson.put("nom", epigraphe.getName());
+		responseJson.put("ImgURL", epigraphe.getImgUrl());
+		String jsonStr = objectMapper.writeValueAsString(responseJson);
+		return Response.ok(jsonStr, MediaType.APPLICATION_JSON).build();
+	}
 }
