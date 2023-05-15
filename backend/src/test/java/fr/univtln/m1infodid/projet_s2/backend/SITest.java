35eb50e56fe32f2dc7df9f6206f2ffbfc82c55aa
khebtani <khebtanidorsaf@hotmail.com>
2023-05-15 11:35:25 +0200

    Factorisation SI

--- backend/src/test/java/fr/univtln/m1infodid/projet_s2/backend/SITest.java
+++ backend/src/test/java/fr/univtln/m1infodid/projet_s2/backend/SITest.java
@@ -3,19 +3,24 @@ package fr.univtln.m1infodid.projet_s2.backend;
 import fr.univtln.m1infodid.projet_s2.backend.exceptions.ListeVide;
 import fr.univtln.m1infodid.projet_s2.backend.model.Epigraphe;
 import org.junit.jupiter.api.Test;
-
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.w3c.dom.Document;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SITest {
     String id = "340";
     String xmlUrl = "http://ccj-epicherchel.huma-num.fr/interface/fiche_xml2.php?id=" + id;
-    List<List<String>> contentdImageEtText = SI.extractTextAndImageFromXml(id, xmlUrl);
+    List<List<String>> contentdImageEtText = SI.extractContentFromXML(id, xmlUrl);
 
     SITest () throws Exception {
     }
@@ -31,7 +36,6 @@ class SITest {
 
     @Test
     void testExtractTextAndImageFromXmlSize () {
-
         // Vérification que la taille de la liste est correcte
         assertEquals(6, contentdImageEtText.size());
     }
@@ -42,31 +46,84 @@ class SITest {
         String regex = "^https?://.+";
         assertEquals(true,contentdImageEtText.get(3).matches(regex) );
     }*/
+
     @Test
     void testExtractTextAndImageFromXmlOthers () {
         // Vérification des autres elements
         for (int i = 0; i < 5; i++) {
-            assertNotNull(contentdImageEtText.get(i).get(0));
+            assertNotNull(contentdImageEtText.get(i));
         }
     }
 
     @Test
     void testExtractTextAndImageFromXmlValidOutput () {
         assertEquals("http://ccj-epicherchel.huma-num.fr/interface/phototheque/340/113594.jpg", contentdImageEtText.get(3).get(0));
-        //assertEquals("TALIS ▴ ER ▴ AT", contentdImageEtText.get(1));
-        //assertEquals("Tel il était!", contentdImageEtText.get(2));
     }
 
-    /*@Test
-    void testExtractTextAndImageFromXmlInvalidUrl() throws Exception {
-        String id = "340";
-        String invalidUrl = "http://ccj-epicherchel.huma-num.fr/invalid-url";
-        ArrayList<String> contentdImageEtText = SI.extractTextAndImageFromXml(id, invalidUrl);
-        assertTrue(contentdImageEtText.isEmpty());
-    }*/
+    @Test
+    public void testGetXMLFromUrl() throws Exception {
+        String xmlUrl = "http://ccj-epicherchel.huma-num.fr/interface/phototheque/340/113594.jpg";
+        InputStream result = SI.getXMLFromUrl(xmlUrl);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testCreateXMLDoc() throws ParserConfigurationException, SAXException, IOException {
+        String xml = "<root><element>test</element></root>";
+        InputStream inputStream = new ByteArrayInputStream(xml.getBytes());
+
+        Document result = SI.createXMLDoc(inputStream);
+        assertNotNull(result);
+
+        assertTrue(result.getDocumentElement().getNodeName().equals("root"));
+    }
+
+    @Test
+    public void testExtraction() throws ParserConfigurationException, IOException, SAXException {
+        List<List<String>> contentList = new ArrayList<>();
+        List<String> transcriptionList = new ArrayList<>();
+        String xmlString = "<root><persName>Adaline</persName></root>";
+
+        InputStream inputStream = new ByteArrayInputStream(xmlString.getBytes());
+        Document doc = SI.createXMLDoc(inputStream);
+
+        Element root = doc.getDocumentElement();
+        Element persName = (Element)root.getElementsByTagName("persName").item(0);
+
+        SI.extraction(contentList, transcriptionList, persName, "", persName.getChildNodes(), 0);
+
+        assertEquals(1, contentList.size());
+        assertEquals(0, transcriptionList.size());
+        assertEquals("Adaline", contentList.get(0).get(0));
+    }
+
+
+    @Test
+    public void testExtractFromBalise() throws ParserConfigurationException, SAXException, IOException {
+        String xmlString = "<root>\n" +
+                "  <test1 type=\"translation\">Contenu 1</test1>\n" +
+                "  <test2 when=\"date\">Contenu 2</test2>\n" +
+                "  <persName>Adaline</persName>\n" +
+                "</root>\n";
+        InputStream inputStream = new ByteArrayInputStream(xmlString.getBytes());
+        Document doc = SI.createXMLDoc(inputStream);
+
+        List<List<String>> contentList = new ArrayList<>();
+        List<String> transcriptionList = new ArrayList<>();
+        String id = "1";
+        List<List<String>> result = SI.extractFromBalise(contentList, transcriptionList, doc, id);
+
+        assertEquals(5, result.size());
+        assertEquals(id, result.get(0).get(0));
+        assertEquals("Contenu 1", result.get(1).get(0));
+        assertEquals("Contenu 2", result.get(2).get(0));
+        assertEquals("Adaline", result.get(3).get(0));
+    }
+
+
     @Test
     void testCreateEpigraphie () throws ParseException, ListeVide {
-        List<List<String>> contentList = new ArrayList<>();
+        List<List<String>>  contentList = new ArrayList<>();
         contentList.add(List.of("42"));
         contentList.add(List.of("philippe"));
         contentList.add(List.of("2022-05-05"));
@@ -85,7 +142,6 @@ class SITest {
 
         Epigraphe resultEpigraphe = SI.CreateEpigraphie(contentList);
 
-        assertEquals(Integer.parseInt(contentList.get(0).get(0)), expectedEpigraphe.getId());
         assertEquals(resultEpigraphe.getId(), expectedEpigraphe.getId());
         assertEquals(resultEpigraphe.getDate(), expectedEpigraphe.getDate());
         assertEquals(resultEpigraphe.getName(), expectedEpigraphe.getName());
