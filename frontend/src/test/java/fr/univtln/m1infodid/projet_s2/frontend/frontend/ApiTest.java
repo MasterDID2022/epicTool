f816db88cc0260323119d9fab11a7800362bbecf
Tom D <tom-domenge@etud.univ-tln.fr>
2023-05-16 08:19:59 +0200

    Prise en charge de l'authentification par l'API
    Co-authored-by: Seraphin Noche <seraphin.noche@yahoo.fr>

--- frontend/src/test/java/fr/univtln/m1infodid/projet_s2/frontend/frontend/ApiTest.java
+++ frontend/src/test/java/fr/univtln/m1infodid/projet_s2/frontend/frontend/ApiTest.java
@@ -3,19 +3,22 @@ package fr.univtln.m1infodid.projet_s2.frontend.frontend;
 import fr.univtln.m1infodid.projet_s2.frontend.server.Api;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import java.util.Base64;
 import java.util.List;
 
-public class ApiTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+  class ApiTest {
 
     /*
     Cette méthode teste la méthode sendRequestOf() de la classe Api en envoyant une requête avec un ID valide.
     *Elle vérifie que le résultat n'est pas nul.
      */
     @Test
-    public void testSendRequestOf() {
+      void testSendRequestOf() {
         Integer id = 1;
         List<String> result = Api.sendRequestOf(id);
         Assertions.assertNotNull(result);
@@ -26,7 +29,7 @@ public class ApiTest {
     *Elle vérifie que le résultat est une chaîne vide.
      */
     @Test
-    public void testSendRequestOfWithInvalidId() {
+     void testSendRequestOfWithInvalidId() {
         Integer id = -1;
         List<String> result = Api.sendRequestOf(id);
         assertEquals(0, result.size());
@@ -37,7 +40,7 @@ public class ApiTest {
     *Elle vérifie que le résultat n'est pas nul.
      */
     @Test
-    public void testPostAnnotations() {
+     void testPostAnnotations() {
         String annotationsJson = "{\"annotations\": []}";
         String result = Api.postAnnotations(annotationsJson);
         Assertions.assertNotNull(result);
@@ -48,7 +51,7 @@ public class ApiTest {
     *Elle vérifie que le résultat est une chaîne vide.
      */
     @Test
-    public void testPostAnnotationsWithInvalidJson() {
+     void testPostAnnotationsWithInvalidJson() {
         String annotationsJson = "{\"annotations\" []}";
         String result = Api.postAnnotations(annotationsJson);
         assertEquals("", result);
@@ -59,7 +62,7 @@ public class ApiTest {
      *Elle vérifie que le résultat est une chaîne vide.
      */
     @Test
-    public void testPostAnnotationsWithNullJson() {
+     void testPostAnnotationsWithNullJson() {
         String annotationsJson = null;
         String result = Api.postAnnotations(annotationsJson);
         assertEquals("", result);
@@ -69,7 +72,7 @@ public class ApiTest {
     *Test réussi - vérification de la valeur de retour
      */
     @Test
-    public void testSendRequestOfSuccess() {
+     void testSendRequestOfSuccess() {
         Integer id = 42;
         List<String> expectedResult = List.of("étude Philippe Leveau", 
                                                 "Thu Jul 12 00:00:00 GMT 2018", 
@@ -89,9 +92,35 @@ public class ApiTest {
     *Test en cas d'échec de la requête (404 Not Found)
      */
     @Test
-    public void testSendRequestOfRequestFailure() {
+     void testSendRequestOfRequestFailure() {
         Integer id = 100000;
         List<String> actualResult = Api.sendRequestOf(id);
         assertEquals(0, actualResult.size());
     }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Tom@gmail.com:/ ,{[39?",
+            "Tom@gmail.com:/: ,{[39?",
+            "Tom@gmail.com:Seraph"
+    })
+    void testPostLoginOK(String credentials) {
+       String loginCredential = Base64.getEncoder().encodeToString((credentials).getBytes());
+       String response = Api.postLogin(loginCredential);
+       assertFalse(response.isEmpty());
+       assertEquals("Bienvenue Tom@gmail.com !", response);
+    }
+
+    @Test
+     void testpostEmptyPassword(){
+        String loginCredential = Base64.getEncoder().encodeToString( "Tom@gmail.com:".getBytes());
+        String reponse = Api.postLogin(loginCredential);
+        assertTrue(reponse.isEmpty());
+    }
+
+
+
+
+
+
 }
