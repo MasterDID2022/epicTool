b0a97b7297e7e5221099ac93af41fa8deb57c746
Tom D <tom-domenge@etud.univ-tln.fr>
2023-05-17 15:05:13 +0200

    Retrait d'une méthode dépréciée

--- backend/src/test/java/fr/univtln/m1infodid/projet_s2/backend/model/AnnotationTest.java
+++ backend/src/test/java/fr/univtln/m1infodid/projet_s2/backend/model/AnnotationTest.java
@@ -7,12 +7,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class AnnotationTest {
     @Test
     void retourneAnnotation(){
-        assertEquals(Annotation.of(1,1).getClass(),Annotation.class);
+        assertEquals(Annotation.of(1).getClass(),Annotation.class);
     }
 
     @Test
     void ajoutPointTest(){
-        Annotation annotation = Annotation.of(1,1);
+        Annotation annotation = Annotation.of(1);
         int avantChangement = annotation.getListCoordonesPoints().size();
         annotation.addPoints(1,1);
         assertEquals(avantChangement+1,annotation.getListCoordonesPoints().size());
